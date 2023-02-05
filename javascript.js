9f37fde5b4fc17c7a545d69c1352cfdb94cda975
Pat Bogut <117064507+PBNJ0613@users.noreply.github.com>
2023-02-05 18:16:11 -0500

    Change initional getComputerChoice function to work without passing argument

--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
 // write function getComputerChoice that will return rock, paper, or scissors
-function getComputerChoice(answer) {
-  return answer[Math.floor(Math.random() * answer.length)];
-}
 
-console.log(getComputerChoice(["rock", "paper", "scissors"]));
+const getComputerChoice= () => {
+  const randomNumber = (Math.floor(Math.random() * 3))
+  if (randomNumber === 0){
+    return 'Rock';
+  }
+  else if (randomNumber === 1){
+    return 'Paper';
+  }
+  else if (randomNumber === 2){
+  return 'Scissors';
+  }
+    }
+console.log(getComputerChoice());
+//write function playRound that plays a single round of the game with user and computer input 
+
+function playRound(playerSelection, computerSelection) {
+  
+}
