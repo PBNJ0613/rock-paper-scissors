b46f2b461ce6c20738c052af8007de5db57d0d6f
Pat Bogut <117064507+PBNJ0613@users.noreply.github.com>
2023-02-05 19:16:18 -0500

    first round of game works correctly and prints result of computerSelection to console

--- javascript.js
+++ javascript.js
@@ -1,20 +1,33 @@
 // write function getComputerChoice that will return rock, paper, or scissors
 
-const getComputerChoice= () => {
-  const randomNumber = (Math.floor(Math.random() * 3))
-  if (randomNumber === 0){
-    return 'Rock';
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
   }
-  else if (randomNumber === 1){
-    return 'Paper';
-  }
-  else if (randomNumber === 2){
-  return 'Scissors';
-  }
-    }
-console.log(getComputerChoice());
-//write function playRound that plays a single round of the game with user and computer input 
+}
+
+//write function playRound that plays a single round of the game with user and computer input
 
 function playRound(playerSelection, computerSelection) {
-  
+  if (playerSelection === computerSelection) {
+    return `It's a tie! you both picked ${playerSelection}`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper";
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
 }
+
+const playerSelection = prompt("Rock, paper, scissors");
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
