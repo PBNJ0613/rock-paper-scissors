74c112f537e73b70841fded3e0c78c56c238f396
Pat Bogut <117064507+PBNJ0613@users.noreply.github.com>
2023-02-07 10:08:16 -0500

    Modified version of program using arrow functions Still certain bugs such as null or cancel or empty string returns undefined and updates score

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@
 let playerScore = 0;
 let compScore = 0;
 
-function getComputerChoice() {
+const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
     return "rock";
@@ -12,49 +12,63 @@ function getComputerChoice() {
   } else if (randomNumber === 2) {
     return "scissors";
   }
-}
+};
 
 //write function playRound that plays a single round of the game with user and computer input
 
-
-
 // function game will call playRound function 5 times and record results and announce winner
-
-
-function game() {
-  for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
-    function playRound(playerSelection, computerSelection) {
-      if (playerSelection === computerSelection) {
-        return `It's a tie! you both picked ${playerSelection}`;
-      } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore ++;
-        return "You win! Rock beats Scissors";
-      } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore ++;
-        return "You win! Paper beats Rock";
-      } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore ++;
-        return "You win! Scissors beats Paper";
-      } else {
-        compScore ++;
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-      }
-    }
-    
-    const playerSelection = prompt("Rock, paper, scissors");
-    const computerSelection = getComputerChoice();
-    
-    
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    const score = "Player Score: " + playerScore + " CPU Score: " + compScore;
-    console.log(score); }
+function userChoice() {
+  let userChoice = prompt("Do you chose rock, paper, or scissors?");
+  userChoice = userChoice.toLowerCase();
+  if (
+    userChoice == "rock" ||
+    userChoice == "paper" ||
+    userChoice == "scissors"
+  ) {
+    return userChoice;
+  } else if (userChoice === "" || userChoice === null) {
+    alert("Enter an answer"); 
+  } else {
+    alert("not a valid choice");
+  }
 }
 
+const playRound = (selection, computerSelection) => {
+  if (selection === computerSelection) {
+    return `It's a tie! you both picked ${selection}`;
+  } else if (selection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    return "You win! Rock beats Scissors";
+  } else if (selection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    return "You win! Paper beats Rock";
+  } else if (selection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    return "You win! Scissors beats Paper";
+  } else {
+    compScore++;
+    return `You lose! ${computerSelection} beats ${selection}`;
+  }
+};
+
+const game = () => {
+  for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
+    const computerSelection = getComputerChoice();
+    let selection = userChoice();
+
+    let result = playRound(selection, computerSelection);
+    console.log(result);
+    const score = "Player Score: " + playerScore + " CPU Score: " + compScore;
+    console.log(score);
+  }
+};
+
 game();
- 
+
 if (playerScore > compScore) {
   console.log("winner");
 } else if (playerScore < compScore) {
   console.log("loser");
-} else {console.log("tie")}
+} else {
+  console.log("tie");
+}
